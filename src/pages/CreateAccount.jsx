8fa4288672d8d4ee27dd692061d02bd91d884f05
pheebsloyola <pheebs.avion@gmail.com>
2024-11-28 20:57:08 +0800

    Create Account Design Added

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import axios from "axios";
 import { API_URL } from "../constants/Constants";
 import { useNavigate } from "react-router-dom";
+import "./CreateAccount.css";
 
-function CreateAccount(){
+function CreateAccount() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirmation, setPasswordConfirmation] = useState("");
@@ -17,18 +18,18 @@ function CreateAccount(){
                 email,
                 password,
                 passwordConfirmation
-              }
+              };
     
           const response = await axios.post(`${API_URL}/auth`, userDetails);
           const { data, headers } = response;
           if(data && headers) {
-            const accessToken = headers["access-token"];
-            const expiry = headers["expiry"];
-            const client = headers["client"];
-            const uid = headers["uid"];
+            // const accessToken = headers["access-token"];
+            // const expiry = headers["expiry"];
+            // const client = headers["client"];
+            // const uid = headers["uid"];
     
-            console.log(data);
-            console.log(accessToken, expiry, client, uid);
+            // console.log(data);
+            // console.log(accessToken, expiry, client, uid);
     
             // handleHeaders(headers);
     
@@ -42,33 +43,54 @@ function CreateAccount(){
         }
       };
  return(
-     <div>
-          <form onSubmit={handleSubmit}>
-       <input
-       type="text"
-       value={email}
-       onChange={(e) => setEmail(e.target.value)}
-       placeholder="Email address"
-       required
-       />
-       <input
-       type="password"
-       value={password}
-       onChange={(e) => setPassword(e.target.value)}
-       placeholder="Password"
-       required
-       />
-        <input
-       type="password"
-       value={passwordConfirmation}
-       onChange={(e) => setPasswordConfirmation(e.target.value)}
-       placeholder="Confirm password"
-       required
-       />
-       <button className="btn btn-success" type="submit">Create account</button>
-          </form>
-     </div>
- )
+  <div className="create-account-container">
+  <img
+    className="logo"
+    src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png"
+    alt="Slack Logo"
+  />
+  <h1>Create Your Account</h1>
+  <p>Enter your email and password to create a new account.</p>
+  <form onSubmit={handleSubmit}>
+    <div className="input-group">
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email address"
+        required
+      />
+    </div>
+    <div className="input-group">
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+      />
+    </div>
+    <div className="input-group">
+      <input
+        type="password"
+        value={passwordConfirmation}
+        onChange={(e) => setPasswordConfirmation(e.target.value)}
+        placeholder="Confirm password"
+        required
+      />
+    </div>
+    <button className="btn btn-primary" type="submit">
+      Create Account
+    </button>
+  </form>
+  <p className="footer-text">
+    Already have an account?{" "}
+    <a href="/login" className="link">
+      Sign in here.
+    </a>
+  </p>
+</div>
+);
 }
 
 export default CreateAccount;
